795688a4bbee31958e8251c74fcb8f7b87bef2f6
toconn <tuc.github@uaconaill.com>
2022-12-14 22:25:06 -0500

    Fixed incorrect Windows user home environment variable.

--- src-tauri/lib-shared/src/os/windows/constants.rs
+++ src-tauri/lib-shared/src/os/windows/constants.rs
@@ -3,5 +3,5 @@ pub const NEWLINE: &str = "\r\n";
 pub const OS_NAME: &str = "Windows";
 pub const PATH_SEPARATOR: &str = ";";
 pub const USER_APP_SETTINGS_DIR_VARIABLE: &str = "APPDATA"; // C:\Users\UserName\AppData\Roaming
-pub const USER_HOME_DIR_VARIABLE: &str = "HOME"; // C:\Users\UserName
+pub const USER_HOME_DIR_VARIABLE: &str = "HOMEPATH"; // C:\Users\UserName
 pub const USER_DOC_SUBDIR: &str = "Documents"; // C:\Users\UserName\Documents
