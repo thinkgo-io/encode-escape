f0f9708cc34192e841c2994f5cb7e2472103381c
thinkgo-io <tg+github@thinkgo.io>
2023-02-08 21:15:04 -0500

    Ours goes to 11.

--- src-tauri/src/data/app_info.rs
+++ src-tauri/src/data/app_info.rs
@@ -1,5 +1,5 @@
 pub const APP_NAME: &str = "Encode Escape";
-pub const APP_VERSION: &str = "1.0.5";
+pub const APP_VERSION: &str = "1.0.11";
 pub const APP_CREATED_DATE: &str = "2022-10-17";
-pub const APP_BUILD_DATE: &str = "2023-02-07";
-pub const APP_BUILD_NUMBER: &str = "4";
+pub const APP_BUILD_DATE: &str = "2023-02-08";
+pub const APP_BUILD_NUMBER: &str = "10";
